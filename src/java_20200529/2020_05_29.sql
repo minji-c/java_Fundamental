a2fc50d56e13745168dd5812f1db104cfc95761c
minji-c <minji.cho.20@gmail.com>
2020-05-29 17:44:05 +0900

    2020_05_29

--- src/java_20200529/2020_05_29.sql
+++ src/java_20200529/2020_05_29.sql
@@ -1,4 +1,21 @@
-#emp 테이블에서 사원번호, 사원이름, 직업을 출력
+CREATE TABLE member(
+num INT PRIMARY KEY,
+NAME VARCHAR(50),
+addr VARCHAR(50));
+
+DESC member;
+
+INSERT INTO member (num, NAME, addr) VALUES (1,"성영한",'서울');
+INSERT INTO member (num, NAME) VALUES(2,'손정의');
+INSERT INTO member VALUES(3,'잡스','미국');
+#에러 발생 - primary key 칼럼에는 중복된 값이 들어가면 안됨
+INSERT INTO member (num,NAME, addr) VALUES(1,'조스','미국');
+#에러 발생 - primary key 칼럼에는 값이 반드시 있어야 함.
+INSERT INTO member (NAME, addr) VALUES('조스','미국');
+
+SELECT * FROM member;
+
+#emp 테이블에서 사원번호, 사원이름, 직업을 출력. 
 SELECT empno, ename, job
 FROM emp;
 
@@ -6,78 +23,101 @@ FROM emp;
 #단, 급여가 많은 순서대로 출력
 SELECT empno, sal, deptno
 FROM emp
-ORDER BY sal DESC;
+ORDER BY sal;
 
 #emp 테이블 에서 사원번호,급여,입사일을 출력. 
 #단,급여가 적은 순서대로.
+
 SELECT empno, sal, hiredate
 FROM emp
-ORDER BY sal;
+ORDER BY sal ASC
 
 #emp 테이블에서 직업,급여를 출력. 
 #단,직업명으로 오름차순, 급여로 내림차순 정렬해서
+
 SELECT job, sal
 FROM emp
 ORDER BY job ASC, sal DESC;
 
-#emp 테이블에서 empno는 사원번호로 ename 사원이름으로
-#컬럼정보를 바꿔서 출력하시오.
-#AS 와 큰따옴표 " 는 생략가능 하다(공백문자가 있으면 안됨 => 사원 번호) 
-SELECT empno AS "사원번호", ename AS "사원이름" 
+#emp 테이블에서 empno는 사원번호로 ename 사원이름 으로
+#컬럼 정보를 바꿔서 출력하시오
+
+SELECT empno AS "사원 번호", ename AS "사원 이름"
 FROM emp;
 
-SELECT empno 사원번호, ename 사원이름 
+SELECT empno AS 사원번호, ename AS 사원이름
+FROM emp;
+
+SELECT empno 사원번호, ename 사원이름
 FROM emp;
 
 SELECT empno "사원 번호", ename "사원 이름"
 FROM emp;
 
-#emp 테이블에서 부서번호가 10 번인 사원들의 급여와 
-#10% 인상된 금액 같이 출력
-SELECT sal, sal*1.1
+#emp 테이블에서 부서번호가 10 번인 사원들의 부서번호와 
+#급여와 10% 인상된 금액을  출력
+SELECT deptno , sal , sal * 1.2
 FROM emp
-WHERE deptno=10;
+WHERE deptno = 10
 
-#emp 테이블에서 부서번호가 10 번인 사원들의 부서번호와
-급여와 급여+커미션 출력
-SELECT deptno, sal, sal+ifnull(comm,0)
+#emp 테이블에서 부서번호가 10 번인 사원들의 부서번호와 
+#급여와 급여+커미션  출력
+
+
+SELECT deptno , sal , sal + ifnull(comm,0)
 FROM emp
-WHERE deptno=10;
+WHERE deptno = 10;
 
-#emp 테이블에서 급여가 3000 이상인 사원들의 모든 정보를 출력.
+#emp 테이블에서 급여가 3000 이상인 사원들의 
+#모든 정보를 출력
+
 SELECT empno, ename, job, mgr, hiredate, sal, comm, deptno
 FROM emp
-WHERE sal>=3000;
+WHERE sal >= 3000;
 
-#emp 테이블에서 부서번호가 30번이 아닌 사람들의 이름과 부서번호를 출력.
-select ename, deptno
+#emp 테이블에서 부서번호가 30번이 아닌 사람들의 
+#이름과 부서번호를 출력해보세요. 
+
+SELECT ename, deptno
 FROM emp
-WHERE deptno!=30;
+WHERE deptno != 30;
 
-#emp 테이블에서 부서번호가 10번이고 급여가 3000 이상인 
-#사원들의 이름과 급여를 출력.
+#emp 테이블에서 부서번호가 10번이고 
+#급여가 3000 이상인 사원들의 이름과 급여를 출력. 
+
 SELECT ename, sal
 FROM emp
-WHERE deptno=10 AND sal>=3000;
+WHERE deptno = 10 AND sal >=3000 ;
 
-#emp 테이블에서 직업이 SALESMAN 이거나 MANAGER 인 
-#사원의 사원번호와 부서번호를 출력. 
-SELECT empno,deptno, job
+
+#emp 테이블에서 직업이 SALESMAN 이거나 
+#MANAGER 인 사원의 사원번호와 부서번호를 출력
+
+SELECT empno, deptno , job
 FROM emp
-WHERE job='salesman' OR job='manager';
+WHERE job = 'salesman' OR job = 'manager' ;
 
 SELECT empno, deptno, job
 FROM emp
-WHERE job != 'clerk' AND job != 'analyst' AND job != 'president';
-
-#부서번호가 10번이거나 20번인 사원의
-#사원번호와 이름, 부서번호 출력.
-
-SELECT empno, ename, deptno
+WHERE job = 'salesman'
+UNION ALL
+SELECT empno, deptno, job
 FROM emp
-WHERE deptno= 10 OR deptno= 20; 
+WHERE job = 'manager'
 
-SELECT ename,deptno
+SELECT empno, deptno, job
+FROM emp
+WHERE job != 'clerk' AND job != 'analyst' 
+AND job != 'president';
+
+#부서번호가 10번이거나 20번인 사원의 
+#사원번호와 이름, 부서번호 출력. 
+
+SELECT ename, deptno
+FROM emp
+WHERE deptno = 10 OR deptno = 20;
+
+SELECT ename, deptno
 FROM emp
 WHERE deptno IN (10,20);
 
@@ -86,7 +126,7 @@ FROM emp
 WHERE deptno != 30;
 
 #급여가 1000 과 2000 사이인 사원들의 
-사원번호,이름,급여를 출력. 
+#사원번호,이름,급여를 출력 
 
 SELECT empno, ename, sal
 FROM emp
@@ -96,55 +136,68 @@ SELECT empno, ename, sal
 FROM emp
 WHERE sal BETWEEN 1000 AND 2000;
 
-#사원이름이 'FORD' 와 'SCOTT' 사이의 사원들의 사원번호,이름을 출력. 
+#사원이름이 'FORD' 와 'SCOTT' 사이의 사원들의 
+#사원번호,이름을 출력. 
 
-SELECT empno, ename
+SELECT deptno, ename
 FROM emp
-WHERE ename => 'ford' AND ename <= 'scott'
+WHERE ename >= 'ford' AND ename <= 'scott'
 ORDER BY ename ASC;
 
- 
-#사원이름이 J 로 시작하는 사원의 사원이름과 부서번호를 출력하기.
+#사원이름이 'J' 로 시작하는 
+#사원의 사원이름과 부서번호를 출력
+
 SELECT ename, deptno
 FROM emp
-WHERE ename LIKE '%J';
+WHERE ename LIKE 'j%'
 
-#사원이름이 J 를 포함하는 사원의 사원이름과 부서번호를 출력하기.
+#사원이름에서  'J' 를 포함하는 
+#사원의 사원이름과 부서번호를 출력하기
+
 SELECT ename, deptno
 FROM emp
-WHERE ename LIKE '%J%';
+WHERE ename LIKE '%j%';
 
-#사원이름의 두번째글자가 A인 사원의 이름,급여,입사일을 출력하기.
+#사원이름의 두번째 글자가 'A' 인 
+#사원의 이름,급여,입사일을 출력
+
 SELECT ename, sal, hiredate
 FROM emp
 WHERE ename LIKE '_A%';
 
-#사원이름이 ES로 끝나는 사원의 이름, 급여, 입사일을 출력하기.
+#사원 이름이 'ES' 로 끝나는 
+#사원의 이름,급여,입사일을 출력해 보세요.
+
 SELECT ename, sal, hiredate
 FROM emp
 WHERE ename LIKE '%ES';
 
-#입사년도가 81년인 사원들의 입사일과 사원번호를 출력하기.
-select ename, hiredate, empno
-from emp
-where hiredate LIKE '1981%';
 
-#커미션이 null인 사원의 사원이름과 커미션을 출력.
+#입사년도가 81년 인 
+#사원들의 입사일과 사원번호를 출력
+
+SELECT ename, hiredate , empno
+FROM emp
+WHERE hiredate LIKE '1981%';
+
+#커미션이 NULL 인 사원의 사원이름과 커미션을 출력. 
+
 SELECT ename, comm
 FROM emp
 WHERE comm IS NULL;
 
-#커미션이 null이 아닌 사원의 사원이름과 커미션을 출력.
+#커미션이 NULL 이 아닌 사원의 사원이름과 커미션을 출력. 
+
 SELECT ename, comm
 FROM emp
-WHERE comm IS NOT NULL;
+WHERE comm IS NOT NULL 
 
-SELECT CONCAT(ename,'그의 직업은','입니다') 
+SELECT CONCAT(ename,'의 직업은', job, ' 입니다.') intro
 FROM emp;
 
 SELECT CHAR(97);
 
-SELECT CONCAT(ename,'him') NAME
+SELECT CONCAT(ename,'님') NAME
 FROM emp;
 
 SELECT LOWER(ename)
@@ -155,116 +208,86 @@ FROM emp;
 
 SELECT LPAD('123', 10, '0');
 
-SELECT RPAD('123', 10, '0');
+SELECT RPAD('123',10,'0');
 
-#length: 영문일때 1, 한글 3byte
-SELECT LENGTH('스미쓰');
+#length : 영문일때 1 btye, 한글 3 byte
+SELECT LENGTH('스 미쓰');
 
 SELECT ename, LEFT(ename,3)
 FROM emp;
 
-SELECT ename
+SELECT ename, rpad(LEFT(ename,3),length(ename),'*')
 FROM emp;
-
-
-#####
-SELECT ename, rpad(LEFT(ename,3),LENGTH(NAME),'*')
-FROM emp;
-
 
 SELECT ename, RIGHT(ename,3)
 FROM emp;
 
-SELECT ename, LPAD(RIGHT(ename,3),LENGTH(ename),'*')
+SELECT ename, lpad(RIGHT(ename,3),length(ename),'*')
 FROM emp;
 
-SELECT ename, SUBSTRING(ename, 3, 3)
+SELECT ename, SUBSTRING(ename, 3,3)
 FROM emp;
 
 SELECT FORMAT(123456.34355,2);
 
-
-#SELECT ename, NVL(comm,0) FROM emp;
-
-SELECT ename, IFNULL(comm,o) 
+SELECT ename, ifnull(comm, 0) 
 FROM emp;
 
-SELECT ABS(-10);
+SELECT ABS(-50000000);
 
-SELECT CEIL(3.1234);
+SELECT CEIL(4.06789);
 
-SELECT FLOOR(3.2241);
+SELECT FLOOR(4.99999);
 
-SELECT ROUND(3.22645,2);
+SELECT ROUND(2345.345,2);
 
-
-
-SELECT ROUND(4.1,2);
-SELECT ROUND(4.15,2);
-
+SELECT MOD(10,3);
 
 SELECT TRUNCATE(123123.123567,3);
 
-SELECT NOW();
 SELECT CURDATE();
+
 SELECT CURTIME();
-SELECT ADDDATE(NOW(),INTERVAL 2 DAY);
-SELECT SUBDATE(NOW(),INTERVAL 2 hour);
 
-SELECT DATEDIFF('2020-05-29','2020-05-16');
-SELECT TIMESTAMPDIFF(HOUR,'2018-03-01','2018-03-28');
+SELECT NOW();
 
 
-
-SELECT CONCAT(ename, '님') 
-FROM emp;
-
+SELECT NOW(), ADDDATE(NOW(), INTERVAL 2 DAY);
 
 CREATE TABLE test(
 id VARCHAR(50) PRIMARY KEY,
-sdate DATETIME;
-edate DATETIME;
+sdate DATETIME,
+edate DATETIME
 );
 
-INSERT INTO test(id, sdate, edate)
-VALUES('syh1011',NOW()),ADDDATE(NOW(), INTERVAL 2 DAY));
+INSERT INTO test(id, sdate, edate) 
+VALUES('syh1011',NOW(), ADDDATE(NOW(), INTERVAL 2 DAY));
 
-INSERT INTO test(id, sdate, edate)
-VALUES('syh2022',NOW()),ADDDATE(NOW(), INTERVAL 20 DAY));
+INSERT INTO test(id, sdate, edate) 
+VALUES('syh2022',NOW(), ADDDATE(NOW(), INTERVAL 20 DAY));
 
-INSERT INTO test(id, sdate, edate)
-VALUES('syh3033',NOW()),ADDDATE(NOW(), INTERVAL 40 DAY));
+INSERT INTO test(id, sdate, edate) 
+VALUES('syh3033',NOW(), ADDDATE(NOW(), INTERVAL 40 DAY));
 
-INSERT INTO test(id, sdate, edate)
-VALUES('syh4044',NOW()),ADDDATE(NOW(), INTERVAL 40 hours));
+
+INSERT INTO test(id, sdate, edate) 
+VALUES('syh4044',NOW(), ADDDATE(NOW(), INTERVAL 40 HOUR));
+
 
 SELECT id, sdate, edate
 FROM test;
 
-SELECT NOW(), SUBDATE(NOW(), INTERVAL 2 HOUR);
+SELECT NOW(), SUBDATE(NOW() , INTERVAL 2 HOUR );
 
-SELECT DATEDIFF('2020-05-29','2020-05-21');
+SELECT DATEDIFF('2020-04-29','2020-05-21');
 
 SELECT id, DATEDIFF(edate, sdate) diff
 FROM test;
 
-SELECT id, TIMESTAMPDIFF(HOUR, sdate, edate)
+SELECT id, TIMESTAMPDIFF(minute, sdate, edate)
 FROM test;
 
-SELECT id, DATE_FORMAT(sdate, '%y/%m/%d') sdate, edate
-DATE_FORMAT(edate, '%Y/%m/%d %h:%i:%s') edate
+SELECT id, date_format(sdate,'%Y/%m/%d') sdate, 
+DATE_FORMAT(edate, '%Y.%m.%d %h:%i:%s') edate , edate
 FROM test;
- 
-
-
-
-
-
-
-
-
-
-
-
-
 
